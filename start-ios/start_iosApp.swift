2e131833b51ca29fd057e47ef9efd44e2a154827
junsuPark <krpjs0508@gmail.com>
2024-11-18 22:26:00 +0900

    initial commit

---
+++ start-ios/start_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  start_iosApp.swift
+//  start-ios
+//
+//  Created by Junsu Park on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct start_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ start-iosTests/start_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  start_iosTests.swift
+//  start-iosTests
+//
+//  Created by Junsu Park on 11/18/24.
+//
+
+import Testing
+@testable import start_ios
+
+struct start_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
